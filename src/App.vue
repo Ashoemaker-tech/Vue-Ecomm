a2dc970149510f0bb163c4b047e85f8d708daad0
ashoemaker-tech <andrew.shoemaker9@gmail.com>
2023-01-18 21:03:39 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Header />
+    <Nav />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Header from './Components/Header.vue'
+import Nav from './Components/Nav.vue'
+import Footer from './Components/Footer.vue'
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <Banner />
+  <Features />
+  <Categories />
+  <NewArrival />
+  <HomeProduct />
+</template>
+
+<script setup>
+import Banner from '../Components/Home/Banner.vue';
+import Categories from '../Components/Home/Categories.vue';
+import Features from '../Components/Home/Features.vue';
+import NewArrival from '../Components/Home/NewArrival.vue';
+import HomeProduct from '../Components/Home/HomeProduct.vue';
+
+
+</script>
